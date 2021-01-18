4b0a736ec36752dbcc4dcf65af42bf122c8a953a
Sean Daly <daly.se91@gmail.com>
2021-01-18 16:39:57 -0600

    Cleaned up indentation and organization in current code

--- src/components/SingleMtn.js
+++ src/components/SingleMtn.js
@@ -2,23 +2,21 @@ import React from "react"
 import Weather from "../components/Weather"
 
 const SingleMtn = ({data}) => {
-    // console.log("This is singlemtn props/state", data?.props.name)
+    
     const loaded =() =>{
 
-        return(<div className="singleMtn">
-        <div> This is the Single moutains component</div>
-        <div>{data.props.name}</div> 
-        <Weather lat={data.props.lat} long={data.props.long}/>
-    </div>
-
+        return(
+            <div className="singleMtn">
+                <div> This is the Single moutains component</div>
+                <div>{data.props.name}</div> 
+                <Weather lat={data.props.lat} long={data.props.long}/>
+            </div>
         )
     }
     const loading=()=>{
         return <></>
     }
-    
-    
-    
+
     return(
         data ? loaded() : loading()
         
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect}  from "react"
 
 const Weather = (props) => {
-    // console.log("This is props", props)
+    
     const apiKey= "bbb59f00c92b004f07e523eaa0647cf7"
     const [weather, setWeather] = useState(null)
     let lat = props.lat
@@ -15,16 +15,14 @@ const Weather = (props) => {
     }
     
     useEffect(()=>getWeather(), [lat, long])  
-    // console.log("This is Snow state", weather)
+    
 
     const WeatherInfo = weather?.map((ele, index)=>{
         return (
             <div key={index}>
-                <div>This is temp</div>
-                <div>This is lat and long</div>
-            <div>This is current Temp: {Math.floor(ele.current.temp)} °F</div>
-            <div>This is the timezone: {ele.timezone}</div>
-            <div>This is the location: {ele.lat} by {ele.lon}</div>
+                <div>This is current Temp: {Math.floor(ele.current.temp)} °F</div>
+                <div>This is the timezone: {ele.timezone}</div>
+                <div>This is the location: {ele.lat} by {ele.lon}</div>
             
             </div>
         )    
