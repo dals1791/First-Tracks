4729f82a80a4fdd1e70d92735abf3266b18b507b
Sean Daly <daly.se91@gmail.com>
2021-01-22 10:42:00 -0600

    added information to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,28 @@
-import React from "react"
+import React from "react";
+// React-Bootstrap Components ---------------------------
+import Container from "react-bootstrap/Container";
 
 const About = () => {
-    return(
-        <div> This is the About component</div>
-    )
-}
-export default About
+  return (
+    <Container className = "about">
+      <p>Deciding on where to ski or snowboard? Mountain Tracker can help!</p>
+
+      <p>
+        Mountain Tracker is an app that allows you to get updates on weather and
+        other information for your go-to ski & snowboard resorts all in one
+        place.
+      </p>
+      The app uses a weather data API created by https://openweathermap.org/api/
+      <Container className="contact">
+          <p>Feel free to contact me @:</p>
+      <ul className="about-list">
+          <li>daly.se91@gmail.com</li>
+          <li><a href="https://github.com/dals1791">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/seandaly0593/">LinkedIn</a></li>
+          <li><a href="https://dals1791.github.io/">And check my protfolio out</a></li>
+      </ul>
+      </Container>
+    </Container>
+  );
+};
+export default About;
